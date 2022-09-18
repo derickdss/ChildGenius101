b7111bedce813bab1fc9bcffce7e363ec45dc15e
derickdsilva <derickdss38@gmail.com>
2022-09-18 13:01:05 +0100

    adding stopwatch comp

---
+++ components/StopWatch.jsx
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+
+const StopWatch = () => {
+    const [time, setTime] = useState(0);
+    const [start, setStart] = useState(false);
+
+    useEffect(()=> {
+        let interval = null;
+
+        if(start) {
+            interval = setInterval(()=> {
+                setTime(prevTime => prevTime + 10)
+            }, 10)
+        } else {
+            clearInterval(interval)
+        }
+
+        return () => clearInterval(interval);
+
+    }, [start])
+
+    const getMinutes = () => ("0" + Math.floor((time / 60000) % 60)).slice(-2)
+    const getSeconds = () => ("0" + Math.floor((time / 1000) % 60)).slice(-2)
+    const getMilliseconds = () => ("0" + ((time / 10) % 1000)).slice(-2)
+
+    return (
+        <div>
+            <h1>Time:</h1>
+            <span>{`${getMinutes()}:${getSeconds()}:${getMilliseconds()}:`}</span>
+            <div>
+                <button onClick={() => setStart(true)}>Start</button>
+                <button onClick={() => setStart(false)}>Stop</button>
+                <button onClick={() => {setTime(0); setStart(false);}}>Reset</button>
+            </div>
+        </div>
+    )
+}
+
+export default StopWatch;
