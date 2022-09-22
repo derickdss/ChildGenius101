68ce286977de790c182630f2fed02c3d28e893c1
derickdsilva <derickdss38@gmail.com>
2022-09-22 11:30:23 +0100

    correcting typo

--- components/PracticeChallenge.jsx
+++ components/PracticeChallenge.jsx
@@ -1,6 +1,6 @@
 import { View, Button } from "react-native";
 
-export default function Home({ navigation, route }) {
+export default function PracticeChallenge({ navigation, route }) {
     return (
         <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
